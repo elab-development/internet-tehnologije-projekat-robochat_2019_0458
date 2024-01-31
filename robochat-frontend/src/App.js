d8b801917413bd2b4c95e6ad2791a0647041a53b
Maja Markovic <markovicmaja384@gmail.com>
2024-01-31 13:28:59 +0100

    Ubacena pocetna stranica sa Pexels API-jem za generisanje slika

--- robochat-frontend/src/App.js
+++ robochat-frontend/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Pocetna from './components/pocetna/Pocetna';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar></Navbar>
-      <Routes>
-
+      <Routes>                
+        <Route path="/" element={<Pocetna/>} />
       </Routes>
       </BrowserRouter>
 
